cd62a2d9c6f45cdb5dca41be185aef56a5b6da91
[j.wilkin] <14888851+jo-wilkin@users.noreply.github.com>
2021-10-19 16:17:14 -0700

    Added date input selector - both UI and server functions working.

--- app.R
+++ app.R
@@ -10,47 +10,57 @@ library(plotly)
 # MTC data is provided in XLS format from MTC website (https://mtc.ca.gov/tools-resources/data-tools/monthly-transportation-statistics)
 # Golden Gate data is on the web only (https://www.goldengate.org/bridge/history-research/statistics-data/monthly-traffic-crossings/)
 # For now, the two datasets are formatted and merged in Excel - the MTC data is not readable as a dataframe in its current format
-# Cleaning required transposing the data
 
 # Read in transportation data
-#transport_data <- read.csv('monthly-transportation.csv', na.strings = "NULL") %>% clean_names()
 transport_data <- read.csv('monthly-transportation-tidy.csv', na.strings = "NULL", stringsAsFactors = FALSE) %>% clean_names()
 
+# Select the column names from our transport data and set names to them
+# Adds functionality if transport_data changes to easily add in new data columns to the app
 transport_types <- select(transport_data, 2:5) %>% colnames() %>% setNames(c("Bay Bridge", "Other Bridges", "BART", "Golden Gate"))
 
+# Re-read date column in data format
 transport_data$date <- lubridate::my(transport_data$date)
 
-#td <- transport_data %>% select(date, golden_gate) %>% rename(n = golden_gate)
-
+# Plot code test - integrated into server function
 # ggplot(transport_data, aes(x=date, y=golden_gate)) +
 #   geom_line() + 
 #   xlab("")
 
 # Design theme for plots
-
 transport_theme <- bs_theme(
   #bg = "#002B36", fg = "#EEE8D5", primary = "#2AA198", 
+  # Add custom base font from google
   base_font = font_google("Montserrat")
 )
 
-# Begin to build app
+# Begin to build Shiny app
 
+# UI Interface
 ui <- fluidPage(
+  # Add reference to stylesheet in www folder
   tags$head(
     tags$link(rel = "stylesheet", type = "text/css", href = "custom.css")
   ),
+  # Add transport theme from above for base font
   theme = transport_theme,
+  # Rename title panel - need to figure out removing H1 from the tab in browser
   titlePanel(h1("Monthly Transportation Statistics in the Bay Area")),
+  # Initiate sidebar layout - note, needs siderbar and Main panel
   sidebarLayout(
+    # Start with sidebar panel
     sidebarPanel(
       p("In March 2020, in response to the COVID-19 pandemic, the State of California ordered as residents to Shelter-In-Place.
         The aim of the order was to reduce the spread of the novel coronavirus encouraging all residents who could, to work at home."), 
       p("This visualisation helps to show the impact of this order on travel in the San Francisco Bay Area by looking at key transportation routes and their user statistics."),
       p("You can visualise monthly one-way toll crossing statistics for the Golden Gate Bridge, the Bay Bridge, other Bridges in the SF Bay Area (Antioch, Benicia-Martinez, Carquinez, Dumbarton, Richmond-San Rafael and San Mateo-Hayward bridges)
         and BART ridership statistics (average weekday BART station exits)."),
-      p("Data is available from January 2019 until September 2021 (NB. The Golden Gate Bridge only has data until August 2021)."), 
+      p("Data is available from January 2019 until September 2021"),
+      em("NB. The Golden Gate Bridge only has data until August 2021."),
+      p(""),
+      
       helpText("Choose a transit mode to plot their monthly statistics:"),
       
+      # Input selector for the variables to display
       selectInput("var", 
                   label = "",
                   choices = transport_types,
@@ -58,20 +68,35 @@ ui <- fluidPage(
       
       p("You can also filter the data to a specific data range of interest"), 
       
-      #sliderInput("range", 
-       #           label = "Date Range Of Interest:",
-        #          min = 0, max = 100, value = c(0, 100))
+      # Date Range Input
+      #sliderInput("date_range", label = "Date Range Of Interest:", min = min(transport_data$date) , max = max(transport_data$date), c(min(transport_data$date), max(transport_data$date))),
+      
+      # Select minimum date
+      #tags$div(selectInput("min_date", label = "From:", choices = transport_data$date, selected = min(transport_data$date)), style="display:inline-block"),
+      
+      # Select maximum date
+      #tags$div(selectInput("max_date", label = "To:", choices = transport_data$date, selected = max(transport_data$date)), style="display:inline-block"),
+      
+      fluidRow(
+        column(6, selectInput("min_date", label = "From:", choices = transport_data$date, selected = min(transport_data$date))),
+        column(6, selectInput("max_date", label = "To:", choices = transport_data$date, selected = max(transport_data$date))),
+      ),
     ),
-    
+
     mainPanel(
       plotOutput("graph"),
+        )
       )
-  )
 )
+  
+    
+      
 
 server <- function(input, output, session){
   
   selected <- reactive(transport_data %>% select(date, input$var) %>% rename(n = input$var))
+  
+  y_limit <- reactive()
   
   output$graph <- renderPlot({
     selected() %>% ggplot(aes(date, n)) +
@@ -85,7 +110,7 @@ server <- function(input, output, session){
       #ylim(0, 9000000) +
       scale_y_continuous(
         labels = scales::unit_format(unit = "Million", scale = 1e-6), limits = c(0, 9000000)) +
-      scale_x_date(date_labels = "%b %Y", date_breaks = "3 month", limit=c(as.Date("2019-01-01"),as.Date("2021-09-01"))) +
+      scale_x_date(date_labels = "%b %Y", date_breaks = "2 month", limit=c(as.Date(input$min_date),as.Date(input$max_date))) +
       theme(
         plot.title = element_text(color = "#0099f9", size = 20, face = "bold", hjust = 0.5),
         plot.subtitle = element_text(size = 13, face = "bold", hjust = 0.5),
